13ddf7371f48cb14cdf17e3d32a851e020e025d9
Viktor Nascimento <viktornascimento@protonmail.com>
2023-04-16 11:32:17 -0300

    Cross, circle and the 3x3 grid implemented

--- net/viktornas/gui/GraphicsPanel.java
+++ net/viktornas/gui/GraphicsPanel.java
@@ -1,5 +1,6 @@
 package net.viktornas.gui;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -13,13 +14,22 @@ import javax.swing.Timer;
 public class GraphicsPanel extends JPanel implements ActionListener
 {
 	public int width; public int height;
+	public int spaces[] = new int[9];
 	
 	public GraphicsPanel(int width, int height)
 	{
 		this.width = width; this.height = height;
 		
-		this.setPreferredSize(new Dimension(width, height));
+		this.setPreferredSize(new Dimension(width + 1, height + 1));
 		this.setBackground(Color.BLACK);
+		
+		for(int space : this.spaces) {space = 0;}
+		
+		this.spaces[6] = 1;
+		this.spaces[4] = 2;
+		this.spaces[2] = 1;
+		this.spaces[8] = 2;
+		this.spaces[0] = 1;
 		
 		Timer clock = new Timer(250, this);
 		clock.start();
@@ -32,7 +42,41 @@ public class GraphicsPanel extends JPanel implements ActionListener
 		Graphics2D g2D = (Graphics2D) g;
 		
 		g2D.setColor(Color.WHITE);
-		g2D.drawOval((width / 2) - 100, (height / 2) - 100, 200, 200);
+		
+		int space_index = 0;
+		int row_index = 0;
+		int column_index = 0;
+		int w_scale = width / 3;
+		int h_scale = height / 3;
+		
+		for(int i = 0; i < 9; i++)
+		{
+			g2D.drawRect(w_scale * column_index, h_scale * row_index, w_scale, h_scale);
+			
+			if(this.spaces[space_index] == 1)
+			{
+				g2D.setStroke(new BasicStroke(5));
+				g2D.drawLine(w_scale * column_index, h_scale * row_index,
+						w_scale * (column_index + 1), h_scale * (row_index + 1));
+				g2D.drawLine(w_scale * (column_index + 1), h_scale * row_index,
+						w_scale * column_index, h_scale * (row_index + 1));
+				g2D.setStroke(new BasicStroke(1));
+			}
+			else if(this.spaces[space_index] == 2)
+			{
+				g2D.setStroke(new BasicStroke(5));
+				g2D.drawOval(w_scale * column_index, h_scale * row_index, w_scale, h_scale);
+				g2D.setStroke(new BasicStroke(1));
+			}
+			space_index++;
+			
+			column_index++;
+			if(column_index >= 3)
+			{
+				column_index = 0;
+				row_index++;
+			}
+		}
 	}
 	
 	@Override
@@ -40,5 +84,4 @@ public class GraphicsPanel extends JPanel implements ActionListener
 	{
 		super.repaint();
 	}
-
 }
